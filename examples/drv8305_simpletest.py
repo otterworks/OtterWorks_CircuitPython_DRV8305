61dc806667d672d2fced010e22ab08ad72783db9
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2020-08-26 13:49:13 -0700

    use P9_15 as chip select

--- examples/drv8305_simpletest.py
+++ examples/drv8305_simpletest.py
@@ -10,13 +10,16 @@ import digitalio
 import otterworks_drv8305
 
 # on beaglebone black, make sure SPI_1 pins are configured
-subprocess.run(["config-pin", "P9_15", "spi_cs"])
+# subprocess.run(["config-pin", "P9_17", "spi_cs"])
 subprocess.run(["config-pin", "P9_18", "spi"])
 subprocess.run(["config-pin", "P9_21", "spi"])
 subprocess.run(["config-pin", "P9_22", "spi_sclk"])
 
-spi = busio.SPI(board.SCK_1, board.MISO_1, board.MOSI_1)
 cs = digitalio.DigitalInOut(board.P9_15)
+cs.switch_to_output()
+cs.value = False
+
+spi = busio.SPI(board.SCK_1, board.MISO_1, board.MOSI_1)
 drv8305 = otterworks_drv8305.OtterWorks_DRV8305(spi, cs)
 
 while True:
